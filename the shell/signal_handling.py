5c0aeb2cb82cfb624158bede3cc52dde7a872efd
baodongle <baodongle@gmail.com>
2019-04-21 21:26:57 +0700

    add new feature

--- the shell/signal_handling.py
+++ the shell/signal_handling.py
@@ -3,6 +3,23 @@ import signal
 
 
 def handling_signals():
-    signal.signal(signal.SIGQUIT, signal.SIG_IGN )
+    """
+    handle some effects causing interrupt program.
+    Raises:
+        KeyboardInterrupt: raises some errors in case of the expected directory is
+        not existing, it is a file or some Permission problems.
+    """
+    # handling ctrl + C
+    signal.signal(signal.SIGINT, call_sigint)
+    # ignore ctrl + Q
+    signal.signal(signal.SIGQUIT, signal.SIG_IGN)
+    # ignore ctrl + \
     signal.signal(signal.SIGTERM, signal.SIG_IGN)
-    return
+    # ignore Ctrl + Z
+    signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+
+
+def call_sigint(signal, frame):
+    raise KeyboardInterrupt
+
+
